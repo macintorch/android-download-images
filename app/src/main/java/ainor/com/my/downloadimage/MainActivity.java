098d9304ed228ef51048681d1d1182344ca28853
Ainor Syahrizal <macintorch@gmail.com>
2016-12-21 17:21:58 +0800

    button and image view setup

--- app/src/main/java/ainor/com/my/downloadimage/MainActivity.java
+++ app/src/main/java/ainor/com/my/downloadimage/MainActivity.java
@@ -2,12 +2,22 @@ package ainor.com.my.downloadimage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void getImage (View view){
+        Log.i("Button", "pressed");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button getImageButton = (Button) findViewById(R.id.getImageButton);
+
     }
 }
