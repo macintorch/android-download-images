362bddfa18890a17b211a886a32f353fd6e12ce5
Ainor Syahrizal <macintorch@gmail.com>
2016-12-22 07:46:23 +0800

    press button and imageview display content

--- app/src/main/java/ainor/com/my/downloadimage/MainActivity.java
+++ app/src/main/java/ainor/com/my/downloadimage/MainActivity.java
@@ -1,23 +1,92 @@
 package ainor.com.my.downloadimage;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView downloadedImage;
+
+    public class ImageDownloader extends AsyncTask<String,Void,Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            try {
+
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.connect();
+
+                InputStream inputStream = connection.getInputStream();
+
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                return myBitmap;
+
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
+
     public void getImage (View view){
+        //
+
+        ImageDownloader task = new ImageDownloader();
+
+        Bitmap myImage;
+
+        try {
+            myImage = task.execute("https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Nexus_5X_%28White%29.jpg/1024px-Nexus_5X_%28White%29.jpg").get();
+
+            downloadedImage.setImageBitmap(myImage);
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+
+        } catch (ExecutionException e) {
+
+            e.printStackTrace();
+        }
+
         Log.i("Button", "pressed");
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button getImageButton = (Button) findViewById(R.id.getImageButton);
+
+        downloadedImage = (ImageView) findViewById(R.id.imageView);
 
     }
 }
